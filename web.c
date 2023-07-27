alviroiskandar/gwbsid

commit 770213c9a1d5ef28fcf4ddecf572cdba159a1638

gwbsid: web: Make `handle_client_event()` always return 0
It's not the recv() and send() result that decides whether the program
should stop.

Signed-off-by: Alviro Iskandar Setiawan <alviro.iskandar@gnuweeb.org>

web.c
@@ -2396,7 +2396,8 @@ static int handle_client_event(struct worker *wrk, struct epoll_event *ev)
 		return 0;
 	}
 
-	return _handle_client_event(wrk, ev);
+	_handle_client_event(wrk, ev);
+	return 0;
 }
 
 static int handle_event(struct worker *wrk, struct epoll_event *ev)
@@ -2438,6 +2439,7 @@ static void *run_worker(void *arg)
 			break;
 
 		ret = handle_events(wrk, ret);
+		printf("handle_events() returned %d\n", ret);
 		if (ret < 0)
 			break;
 	}

commit 5628148e44af34357e00cd4f0eec43fa301c5bfd

gwbsid: web: Fix an infity loop bug
Fixes: 770213c9a1d5ef28fcf4ddecf572cdba159a1638 ("gwbsid: web: Make `handle_client_event()` always return 0")
Signed-off-by: Alviro Iskandar Setiawan <alviro.iskandar@gnuweeb.org>

web.c
@@ -2367,17 +2367,13 @@ static int _handle_client_event(struct worker *wrk, struct epoll_event *ev)
 	wrk->kill_current = false;
 	if (ev->events & EPOLLIN) {
 		ret = handle_client_pollin(wrk, cl);
-		if (ret < 0)
-			return ret;
-		if (wrk->kill_current)
+		if (ret < 0 || wrk->kill_current)
 			goto out_close;
 	}
 
 	if (ev->events & EPOLLOUT) {
 		ret = handle_client_pollout(wrk, cl);
-		if (ret < 0)
-			return ret;
-		if (wrk->kill_current)
+		if (ret < 0 || wrk->kill_current)
 			goto out_close;
 	}
 
@@ -2439,7 +2435,6 @@ static void *run_worker(void *arg)
 			break;
 
 		ret = handle_events(wrk, ret);
-		printf("handle_events() returned %d\n", ret);
 		if (ret < 0)
 			break;
 	}